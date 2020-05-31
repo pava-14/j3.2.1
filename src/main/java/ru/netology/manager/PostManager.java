82496ec4fd0cef55bd40baec160f5dc3ae86e6e6
Alexander Pavlenko <pava@bk.ru>
2020-05-31 15:41:34 +0700

    Class PostManager Created

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly1, int count, int offset) {
+        //TODO: Add logic later
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+        //TODO: Add logic later
+    }
+}
